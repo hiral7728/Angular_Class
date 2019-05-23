c88b836d9fc482e02b72a9b232e4c859ee3b43fb
Hiral <hiral7728@gmail.com>
2019-05-23 13:43:58 -0700

    ngFor

--- AllDirectives/AllDirectivesApp/src/app/allDiractive/allDiractive.component.ts
+++ AllDirectives/AllDirectivesApp/src/app/allDiractive/allDiractive.component.ts
@@ -10,5 +10,12 @@ export class AllDiractiveComponent implements OnInit {
   courses = [1, 2, 3];
   movies = [1, 2, 3];
   viewMode = "map";
+  students = [
+    { id: 1, name: "Hiral" },
+    { id: 2, name: "Kinjal" },
+    { id: 3, name: "Rajal" },
+    { id: 4, name: "Rinal" }
+  ];
+
   ngOnInit() {}
 }
